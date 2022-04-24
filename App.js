1d800d4ac72bb13d81e1a0665702d72753111e2a
p0nyqz <ponyquiz@gmail.com>
2022-04-24 11:48:57 +0200

    HomeScreen added

--- App.js
+++ App.js
@@ -1,28 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
 import { store } from "./store"
-
-// 1) Set up REDUX
-//
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
-
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build UBER!</Text>
-        <StatusBar style="auto" />
-      </View>
+        <HomeScreen>Home</HomeScreen>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
