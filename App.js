b2f08c2109700dcf7a15b2278d433ceaf721ba52
p0nyqz <ponyquiz@gmail.com>
2022-04-24 03:02:03 +0200

    Redux init

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
+import { store } from "./store"
 
 // 1) Set up REDUX
 //
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Let's build UBER!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Let's build UBER!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
