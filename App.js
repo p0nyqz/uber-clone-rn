b2cc12bfbaebc8b74012c3f14c1a2c140f3d8ebb
p0nyqz <ponyquiz@gmail.com>
2022-04-25 16:59:43 +0200

    HomeScreen finished

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { Provider } from "react-redux";
 import { store } from "./store"
 import HomeScreen from "./screens/HomeScreen";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <Provider store={store}>
-        <HomeScreen>Home</HomeScreen>
+        <SafeAreaProvider>
+            <HomeScreen />
+        </SafeAreaProvider>
     </Provider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {View, SafeAreaView, Image} from 'react-native'
 import tw from 'twrnc';
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
@@ -10,12 +11,13 @@ const HomeScreen = () => {
             style = {{
                 width: 100,
                 height: 100,
-                resizeMode: 'contain'
+                resizeMode: 'contain',
             }}
             source = {{
                 uri: 'https://links.papareact.com/gzs'
             }}
             />
+            <NavOptions />
         </View>
     </SafeAreaView>
   )
