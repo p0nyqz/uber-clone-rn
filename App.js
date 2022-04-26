18262542b8625b34efda829f272048a737a15552
p0nyqz <ponyquiz@gmail.com>
2022-04-26 03:03:35 +0200

    React Native Navigation implement

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { Provider } from "react-redux";
 import { store } from "./store"
 import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-        <SafeAreaProvider>
-            <HomeScreen />
-        </SafeAreaProvider>
+        <NavigationContainer>
+            <SafeAreaProvider>
+                <Stack.Navigator>
+                    <Stack.Screen name='HomeScreen' component={HomeScreen} options={{
+                        headerShown: false,
+                    }}/>
+                    <Stack.Screen name='MapScreen' component={MapScreen} options={{
+                        headerShown: false,
+                    }}/>
+                </Stack.Navigator>
+            </SafeAreaProvider>
+        </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, FlatList, TouchableOpacity, Image, Text } from 'react-native';
 import tw from 'twrnc';
 import { Icon } from "@rneui/base";
+import {useNavigation} from "@react-navigation/native";
 
 const data = [
     {
@@ -19,6 +20,8 @@ const data = [
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+
   return (
       <FlatList
           data={data}
@@ -26,6 +29,7 @@ const NavOptions = () => {
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
               <TouchableOpacity
+                  onPress={()=> navigation.navigate(item.screen)}
                   style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                   <View>
                       <Image
