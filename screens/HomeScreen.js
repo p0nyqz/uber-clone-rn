ea90f3a2fe41e2bb5699c42395f2251bdcfef71d
p0nyqz <ponyquiz@gmail.com>
2022-04-27 00:21:52 +0200

    Google Autocomplete implement

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import {View, SafeAreaView, Image} from 'react-native'
 import tw from 'twrnc';
-import NavOptions from "../components/NavOptions";
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from
+        'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin} from '../slices/navSlice';
+
 
 const HomeScreen = () => {
-  return (
+    const dispatch = useDispatch();
+
+    return (
     <SafeAreaView style={tw`bg-white h-full`}>
         <View style={tw`p-5`}>
             <Image
@@ -17,6 +25,37 @@ const HomeScreen = () => {
                 uri: 'https://links.papareact.com/gzs'
             }}
             />
+            <GooglePlacesAutocomplete
+                styles={{
+                    container: {
+                        flex: 0,
+                    },
+                    textInput: {
+                        fontSize: 18,
+                    },
+                }}
+                placeholder='Where From?'
+                onPress ={(data,details = null) => {
+                    // console.log(data, details);
+                    dispatch(setOrigin({
+                        location: details.geometry.location,
+                        description: data.description
+                    }))
+                    dispatch(setDestination(null));
+                }}
+                fetchDetails={true}
+                returnKeyType={'search'}
+                enablePoweredByContainer={false}
+                minLength={2}
+                query={{
+                    key: {GOOGLE_MAPS_APIKEY},
+                    language: 'en',
+                }}
+                nearbyPlacesAPI="GooglePlacesSearch"
+                debounce={400}
+            />
+
+
             <NavOptions />
         </View>
     </SafeAreaView>
