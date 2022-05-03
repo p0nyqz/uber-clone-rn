d91656e5e4c8c6369cb53cc70b8f485a4cbd68e4
p0nyqz <ponyquiz@gmail.com>
2022-05-03 02:44:36 +0200

    Keyboard Height Fix

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { KeyboardAvoidingView, Platform } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store"
 import HomeScreen from "./screens/HomeScreen";
@@ -6,21 +7,29 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
-
 export default function App() {
+    const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
         <NavigationContainer>
             <SafeAreaProvider>
-                <Stack.Navigator>
-                    <Stack.Screen name='HomeScreen' component={HomeScreen} options={{
-                        headerShown: false,
-                    }}/>
-                    <Stack.Screen name='MapScreen' component={MapScreen} options={{
-                        headerShown: false,
-                    }}/>
-                </Stack.Navigator>
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    style={{flex:1}}
+                    keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+                >
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name='HomeScreen'
+                            component={HomeScreen}
+                            options={{headerShown: false,}}/>
+                        <Stack.Screen
+                            name='MapScreen'
+                            component={MapScreen}
+                            options={{headerShown: false,}}/>
+                    </Stack.Navigator>
+                </KeyboardAvoidingView>
             </SafeAreaProvider>
         </NavigationContainer>
     </Provider>
