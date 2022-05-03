2f80342476ec64ebe81c5f22a51f17ce506fc99f
p0nyqz <ponyquiz@gmail.com>
2022-05-04 01:38:12 +0200

    HomeScreen Button Added

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import tw from 'twrnc';
 import NavigateCard from "../components/NavigateCard";
@@ -6,11 +6,21 @@ import RideOptionsCard from "../components/RideOptionsCard";
 import Map from '../components/Map';
 // import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Icon } from '@rneui/themed';
+import { useNavigation } from "@react-navigation/native";
 
 const MapScreen = () => {
     const Stack = createNativeStackNavigator();
+    const navigation = useNavigation();
+
   return (
     <View>
+        <TouchableOpacity
+            onPress={() => navigation.navigate('HomeScreen')}
+            style={tw`bg-gray-100 absolute top-16 
+            left-8 z-50 p-3 rounded-full shadow-lg`}>
+            <Icon name="menu" />
+        </TouchableOpacity>
         <View style={tw`h-1/2`}>
             <Map />
         </View>
