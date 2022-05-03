5ef9f793f40f2e21b9b89a2a31e9f5af9838d64d
p0nyqz <ponyquiz@gmail.com>
2022-05-03 23:39:03 +0200

    Google Distance Matrix API

--- components/Map.js
+++ components/Map.js
@@ -1,26 +1,46 @@
 import React, { useEffect } from 'react'
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'twrnc';
-import { useSelector } from "react-redux";
-import { selectDestination } from "../slices/navSlice";
-import MapViewDirections from "react-native-maps-directions";
+import {useDispatch, useSelector} from "react-redux";
+import {selectDestination, selectTravelTimeInformation, setTravelInformation} from "../slices/navSlice";
+// import MapViewDirections from "react-native-maps-directions";
 // import { useSelector } from "react-redux";
 import { selectOrigin } from "../slices/navSlice";
+// import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useRef } from "react";
 
 const Map = () => {
     const origin = useSelector(selectOrigin);
     const destination = useSelector(selectDestination);
     const mapRef = useRef(null);
+    // const dispatch = useDispatch();
 
     useEffect(() => {
-        if (!origin || !destination) return;
+        // if (!origin || !destination) return;
 
         // Zoom and fit to markers
-        mapRef.current.fitToSupplieMarkers(['origin', 'destination'],{
-            edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
-        });
-    }, [origin, destination])
+        // mapRef.current.fitToSupplieMarkers(['origin', 'destination'],{
+        //     edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
+        // });
+
+    }, [origin, destination]);
+
+    useEffect(()=> {
+        if (!origin || !destination) return;
+
+        const getTravelTime = async () => {
+            // fetch('https://maps.googleapi.com/maps/api/distancematrix/json?units=imperial&origin=$(origin.description)&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}'
+            // ).then((res) => res.json())
+            // .then(data => {
+            //     dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+            //    // console.log(data)
+            // });
+        };
+
+        getTravelTime()
+    },[origin, destination])
+    //GOOGLE_MAP_APIKEY
+
 
 
     return (
@@ -41,15 +61,15 @@ const Map = () => {
         }}
         >
 
-        { origin && destination && (
+        { /*origin && destination && (
             < MapViewDirections
                 origin={origin.description}
                 destination={destination.description}
-                apikey={GOOGLE_MAP_APIKEY}
+                // apikey={GOOGLE_MAP_APIKEY}
                 strokeWidth={3}
                 strokeColor='black'
             />
-        )}
+        ) */}
 
         <Marker
             coordinate={{
